2ad1921a1578cfa215255716a28c5fcc8b115697
Nur Rony <pro.nmrony@gmail.com>
2018-05-05 04:08:58 +0600

    add java maven script

---
+++ src/main/java/info/nmrony/app/App.java
@@ -0,0 +1,21 @@
+package info.nmrony.app;
+
+/**
+ * Hello world!
+ */
+public class App
+{
+
+    private final String message = "Hello Jenkins!";
+
+    public App() {}
+
+    public static void main(String[] args) {
+        System.out.println(new App().getMessage());
+    }
+
+    private final String getMessage() {
+        return message;
+    }
+
+}
